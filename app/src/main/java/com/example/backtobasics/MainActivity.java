39c315fbaaebd3f13ecb10cc02b41cbc2759b2b6
unknown <a00516283@gmail.com>
2019-07-12 02:44:13 -0500

    register design ready

--- app/src/main/java/com/example/backtobasics/MainActivity.java
+++ app/src/main/java/com/example/backtobasics/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.backtobasics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button registerButton = (Button) findViewById(R.id.button_register);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent numbersIntent = new Intent(MainActivity.this, RegisterActivity.class);
+
+                // Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/backtobasics/RegisterActivity.java
+++ app/src/main/java/com/example/backtobasics/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.backtobasics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button registerButton = (Button) findViewById(R.id.button_login);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent numbersIntent = new Intent(RegisterActivity.this, MainActivity.class);
+
+                // Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
     }
 }
