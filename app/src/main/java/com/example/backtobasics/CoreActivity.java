cbfaba5200225171c4ca34da4c0d0141fb3f587e
unknown <a00516283@gmail.com>
2019-07-13 17:45:55 -0500

    Make a basic bottom navigation without functionality

---
+++ app/src/main/java/com/example/backtobasics/CoreActivity.java
@@ -0,0 +1,42 @@
+package com.example.backtobasics;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class CoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_core);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            break;
+                        case R.id.nav_favorites:
+                            break;
+                        case R.id.nav_btb:
+                            break;
+                        case R.id.nav_msg:
+                            break;
+                    }
+
+
+                    return true;
+                }
+            };
+}
--- app/src/main/java/com/example/backtobasics/MainActivity.java
+++ app/src/main/java/com/example/backtobasics/MainActivity.java
@@ -14,12 +14,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button registerButton = (Button) findViewById(R.id.button_register);
+        Button loginButton = (Button) findViewById(R.id.button_login);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create a new intent to open the {@link NumbersActivity}
                 Intent numbersIntent = new Intent(MainActivity.this, RegisterActivity.class);
+
+                // Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent numbersIntent = new Intent(MainActivity.this, CoreActivity.class);
 
                 // Start the new activity
                 startActivity(numbersIntent);
